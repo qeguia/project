5cb3dafe039c433b5403789b8c20ed60fe49a815
qin eguia <qyeguia.ieu2025@student.ie.edu>
2026-04-10 13:11:28 +0200

    Feat: coded for separated main eurostat file

---
+++ src/main_eurostat.py
@@ -0,0 +1,22 @@
+from data_cleaning.cleaning import (
+    load_eurostat_data,
+    clean_eurostat_data,
+    prepare_hpi_long,
+)
+from analysis.analysis import build_spain_vs_eu_dataset
+from plot.plot import plot_spain_vs_eu
+
+
+def main():
+    raw_eurostat = load_eurostat_data()
+    clean_eurostat = clean_eurostat_data(raw_eurostat)
+    eurostat_long = prepare_hpi_long(clean_eurostat)
+
+    df_plot = build_spain_vs_eu_dataset(eurostat_long)
+    p = plot_spain_vs_eu(df_plot)
+    print(p)
+    p.show()
+
+
+if __name__ == "__main__":
+    main()
