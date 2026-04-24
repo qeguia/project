0a73d2b300183d8e21509c9dd9b9dff512da1058
qin eguia <qyeguia.ieu2025@student.ie.edu>
2026-04-24 10:22:19 +0200

    feat: handing exceptions for incorrect country codes when using eurostat

--- src/main_eurostat.py
+++ src/main_eurostat.py
@@ -1,4 +1,4 @@
-import argparse # Python's standard library module for handling command-line inputs. Used  so the user can choose countries from the terminal instead of editing the code by hand.
+import argparse # Python's standard library module for handling command-line inputs. Used so the user can choose countries from the terminal instead of editing the code by hand.
 
 from data_cleaning.cleaning import (
     load_eurostat_data,
@@ -29,23 +29,41 @@ def main():
     parser.add_argument('--country2', type=str, default=None) # Optional second country code. Example: --country2 PT
     args = parser.parse_args() # Read the arguments written in the command line.
 
-    raw_eurostat = load_eurostat_data()
-    clean_eurostat = clean_eurostat_data(raw_eurostat)
-    eurostat_long = prepare_hpi_long(clean_eurostat)
+    try:
+        raw_eurostat = load_eurostat_data()
+        clean_eurostat = clean_eurostat_data(raw_eurostat)
+        eurostat_long = prepare_hpi_long(clean_eurostat)
 
-    if args.country1 and args.country2:
-        df_plot = build_country_comparison_dataset(
-            eurostat_long,
-            args.country1,
-            args.country2,
-        )
-        p = plot_country_comparison(df_plot, args.country1, args.country2)
-    else:
-        df_plot = build_spain_vs_eu_dataset(eurostat_long)
-        p = plot_spain_vs_eu(df_plot)
+        if args.country1 and args.country2:
+            valid_countries = set(eurostat_long['geo'].unique())
 
-    print(p)
-    p.show()
+            if args.country1 not in valid_countries:
+                raise ValueError(f"Invalid country code: {args.country1}")
+
+            if args.country2 not in valid_countries:
+                raise ValueError(f"Invalid country code: {args.country2}")
+
+            df_plot = build_country_comparison_dataset(
+                eurostat_long,
+                args.country1,
+                args.country2,
+            )
+            p = plot_country_comparison(df_plot, args.country1, args.country2)
+
+        else:
+            df_plot = build_spain_vs_eu_dataset(eurostat_long)
+            p = plot_spain_vs_eu(df_plot)
+
+        print(p)
+        p.show()
+
+    except ValueError as e:
+        print(f"Error: {e}")
+        print("Tip: Use valid Eurostat country codes (e.g., ES, PT, FR).")
+
+    except Exception as e:
+        print("An unexpected error occurred.")
+        print(e)
 
 
 if __name__ == '__main__':
