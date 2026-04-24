adf577a28bbe836ef41f79479334fb37488c1c8b
qin eguia <qyeguia.ieu2025@student.ie.edu>
2026-04-24 13:11:05 +0200

    Feat: project banner

---
+++ src/banner.py
@@ -0,0 +1,14 @@
+import pyfiglet
+
+def print_banner():
+    # Get the version from the file
+    version = "v1.1.0"
+    # Create the banner text
+    banner = pyfiglet.figlet_format("HousingAffordability " + version, font="slant")
+
+    # Print the header with a border of special characters
+    print("#" * 75)
+    print(banner) # ASCII art for the program name and version
+    print("#" * 75)
+
+print_banner()
